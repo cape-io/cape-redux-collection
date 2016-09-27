cdc9f93fe6f6548e952ad08066e896cde0cc91cd
Kai Curry <kai@cape.io>
2016-09-27 13:11:14 -0500

    fix paths

--- test/lang.spec.js
+++ test/lang.spec.js
@@ -1,16 +1,16 @@
 import test from 'tape'
 
-import { isFavList, isValidListItem } from '../../src/redux/project/lang'
-import { favTitle } from '../../src/redux/project/const'
+import { isFavList, isValidListItem } from '../src/lang'
+import { favTitle } from '../src/const'
 
-test('isValidListItem()', t => {
+test('isValidListItem()', (t) => {
   const val = { actionStatus: 'confirmed' }
   t.ok(isValidListItem(val), 'valid')
   const inval = { actionStatus: 'ended' }
   t.notOk(isValidListItem(inval), 'invalid')
   t.end()
 })
-test('isFavList', t => {
+test('isFavList', (t) => {
   t.ok(isFavList({ title: favTitle }), 'favTitle')
   t.notOk(isFavList({ title: 'boo' }), 'non fav title')
   t.end()
